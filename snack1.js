dccb3dfe38c70577fcc01cbca8e04530ecf125ff
EmanueleIppolito <emanuele2410@gmail.com>
2025-12-09 19:49:34 +0100

    Snack 4 fatto

--- snack1.js
+++ snack1.js
@@ -48,3 +48,10 @@ function eseguiOperazione(num1, num2, callback){
 Scrivi una funzione creaTimer che accetta un tempo (in ms)
 e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".*/
 
+function creaTimer(time){
+    return () => setTimeout(() => console.log("Tempo Scaduto!"), time);
+}
+
+creaTimer(10000)();
+
+//Snack 4 Fatto
